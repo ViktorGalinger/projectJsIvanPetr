c3da0b30f280b57cfa15b4e6b2697bb1fc2077a9
Viktor <example@gmail.com>
2021-02-13 19:13:00 +0600

    Урок 13 (Условия)

--- js/script.js
+++ js/script.js
@@ -1,47 +1,36 @@
-/* Задание на урок:
+"use strict"
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-	 - count - сюда передается ответ на первый вопрос
-	 - movies - в это свойство поместить пустой объект
-	 - actors - тоже поместить пустой объект
-	 - genres - сюда поместить пустой массив
-	 - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-	 - 'Один из последних просмотренных фильмов?'
-	 - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-	 movies: {
-		  'logan': '8.1'
-	 }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
-
-let a = prompt('Один из последних просмотренных фильмов?', '');
-let b = prompt('На сколько оцените его?');
-let c = prompt('Один из последних просмотренных фильмов?', '');
-let d = prompt('На сколько оцените его?');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+/* if (4 == 4) {
+	console.log("ok");
+} else {
+	console.log('no');
+} */
 
 
+/* if (num < 49) {
+	console.log('Error');
+} else if (num > 100) {
+	console.log('Много');
+} else {
+	console.log('Ok');
+}
 
+(num === 50) ? console.log('Ok') : console.log('No');
+ */
 
-console.log(personalMovieDB);
+const num = 50;
+
+switch (num) {
+	case 49:
+		console.log('Неверно');
+		break;
+	case 100:
+		console.log('Неверно');
+		break;
+	case 50:
+		console.log('Верно');
+		break;
+	default:
+		console.log('Не в этот раз!');
+		break;
+}	
