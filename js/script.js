b8c9c582372c3df0b74d669db5a01925e101c0ad
Viktor <example@gmail.com>
2021-02-16 09:42:01 +0600

    Урок 20 (Объекты)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,44 @@
 "use strict";
 
-/* function first() {
-	// Do domething
-	setTimeout(function () {
-		console.log(1);
-	}, 500);
+const options = {
+	name: 'test',
+	width: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red'
+	},
+	makeTest: function () {
+		console.log('test');
+	}
+};
+
+options.makeTest();
+
+const { border, bg } = options.colors; // Деструктиризация
+
+/* console.log(options);
+
+let counter = 0; // Переменная для счетчика
+
+for (let key in options) { // Перебрать все свойства/значения объекта а также значения
+	if (typeof (options[key]) === 'object') { // перебрать все свойства/значения объекта в объекте
+		for (let i in options[key]) {
+			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);  //перебрать все свойства/значения объекта в объек
+			counter++; // Посчитаем количество свойств
+		}
+	} else {
+		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+		counter++; // Посчитаем количество свойств в объекте Будет 3.
+	}
 }
 
-function second() {
-	console.log(2);
+for (let key in options) { // Классческий метод проверить кол-во св-в в объекте
+	counter++;
 }
 
-first();
-second(); */
+console.log(counter); */
 
-function learnJS(lang, callback) { // 1 этап
-	console.log(`Я учу ${lang}`);
-	callback();
-}
+//! Свойства и методы объектов
 
-function done() {
-	console.log('Я прошел этот урок!'); // 2 этап
-}
-
-learnJS('Java Script', done); // 3 этап
+/* console.log(Object.keys(options).length); // Берет объект и на его основании создает массив, в котором все элементы*/
