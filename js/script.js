6097c700f0b18d3b6743fc29a3b0ea30a3400b61
Viktor <example@gmail.com>
2021-02-16 10:22:10 +0600

    Урок 21 (Массивы и псевдомассивы)

--- js/script.js
+++ js/script.js
@@ -1,44 +1,25 @@
 "use strict";
 
-const options = {
-	name: 'test',
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: 'black',
-		bg: 'red'
-	},
-	makeTest: function () {
-		console.log('test');
-	}
-};
+/* const arr = [1, 2, 3, 6, 8];
 
-options.makeTest();
+arr.forEach(function (item, i, arr) {
+	console.log(`${i}: ${item} внутри массива ${arr}`);
+}); */
 
-const { border, bg } = options.colors; // Деструктиризация
+/* arr.pop(); */
+/* arr.push(10); */
 
-/* console.log(options);
+/* console.log(arr); */
 
-let counter = 0; // Переменная для счетчика
-
-for (let key in options) { // Перебрать все свойства/значения объекта а также значения
-	if (typeof (options[key]) === 'object') { // перебрать все свойства/значения объекта в объекте
-		for (let i in options[key]) {
-			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);  //перебрать все свойства/значения объекта в объек
-			counter++; // Посчитаем количество свойств
-		}
-	} else {
-		console.log(`Свойство ${key} имеет значение ${options[key]}`);
-		counter++; // Посчитаем количество свойств в объекте Будет 3.
-	}
+/* for (let i = 0; i < arr.length; i++) { // Перебор через обычный цикл
+	console.log(arr[i]);
 }
 
-for (let key in options) { // Классческий метод проверить кол-во св-в в объекте
-	counter++;
-}
+for (let value of arr) { // Перебор через метод For Of
+	console.log(value);
+} */
 
-console.log(counter); */
-
-//! Свойства и методы объектов
-
-/* console.log(Object.keys(options).length); // Берет объект и на его основании создает массив, в котором все элементы*/
+const str = prompt("", "");
+const produtcs = str.split(", ");
+produtcs.sort();
+console.log(produtcs.join('; '));
