6e97b33d5ff5994bfcd7f939ff854861ad546ada
Viktor <example@gmail.com>
2021-02-16 12:52:05 +0600

    Урок 22 (Передача по ссылке или по значению, Spread оператор (ES6-ES9)

--- js/script.js
+++ js/script.js
@@ -1,25 +1,108 @@
 "use strict";
 
-/* const arr = [1, 2, 3, 6, 8];
+/* let a = 5,
+	b = a;
 
-arr.forEach(function (item, i, arr) {
-	console.log(`${i}: ${item} внутри массива ${arr}`);
-}); */
+b = b + 5;
 
-/* arr.pop(); */
-/* arr.push(10); */
+const obj = {
+	a: 5,
+	b: 1
+}; */
 
-/* console.log(arr); */
+/* const copy = obj; //* Не копирует структуру объекта, а передает ссылку!!!
+copy.a = 10;
 
-/* for (let i = 0; i < arr.length; i++) { // Перебор через обычный цикл
-	console.log(arr[i]);
+console.log(copy);
+console.log(obj); */
+
+/* function copy(mainObj) {
+	let objCopy = {};
+
+	for (let key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+
+	return objCopy;
 }
 
-for (let value of arr) { // Перебор через метод For Of
-	console.log(value);
-} */
+const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
+	}
+};
 
-const str = prompt("", "");
-const produtcs = str.split(", ");
-produtcs.sort();
-console.log(produtcs.join('; '));
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+
+console.log(newNumbers);
+console.log(numbers); */
+
+
+/* const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
+	}
+};
+
+const add = {
+	d: 17,
+	e: 20 
+}; */
+
+/* console.log(Object.assign(number, add)); */
+/* const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add); //20
+console.log(clone); //20
+ */
+
+/* const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'sadasdasd';
+
+console.log(newArray); // ["a", "sadasdasd", "c"]
+console.log(oldArray); // ["a", "b", "c"] */
+
+
+/* Спрет оператор разворачивает все свойства массива */
+const video = ['youtube', 'vimeo', 'rutube'],
+	blogs = ['wordpress', 'livejornal', 'blogger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const num = [2, 5, 7]; //  С него нужно получить свойства и ввести их в качестве аргументы в функции выше!
+
+log(...num); // 2, 5, 7
+
+
+// 4й способ создания поверхностой копии объекта
+
+const array = ['a', 'b'];
+const newArray = [...array];
+console.log(newArray); // ["a", "b"]
+console.log(array); // ["a", "b"]
+
+const q = {
+	one: 1,
+	two: 2
+};
+const newObj = { ...q };
+console.log(newObj); // {one: 1, two: 2}
+console.log(q); //{one: 1, two: 2}
