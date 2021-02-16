6b128aa9364bc345d7ef80ff74be7dc385d072d2
Viktor <example@gmail.com>
2021-02-16 14:42:19 +0600

    Урок 23 (Основы ООП)

--- js/script.js
+++ js/script.js
@@ -1,108 +1,17 @@
 "use strict";
 
-/* let a = 5,
-	b = a;
-
-b = b + 5;
-
-const obj = {
-	a: 5,
-	b: 1
-}; */
-
-/* const copy = obj; //* Не копирует структуру объекта, а передает ссылку!!!
-copy.a = 10;
-
-console.log(copy);
-console.log(obj); */
-
-/* function copy(mainObj) {
-	let objCopy = {};
-
-	for (let key in mainObj) {
-		objCopy[key] = mainObj[key];
-	}
-
-	return objCopy;
-}
-
-const numbers = {
-	a: 2,
-	b: 5,
-	c: {
-		x: 7,
-		y: 4
+const solider = {
+	health: 400,
+	armor: 100,
+	sayHello: function () {
+		console.log("Hello!");
 	}
 };
 
-const newNumbers = copy(numbers);
-newNumbers.a = 10;
+/* jonh.__proto__ = solider;	// ((Устаревший формат)) Установили прототипом(родителем)Джона - солдата!
+console.log(jonh);  //Прототипом получил свойства своего прототипа/родителя 	 */
 
-console.log(newNumbers);
-console.log(numbers); */
+const jonh = Object.create(solider); // Мы создаем новый прототип Джон, который будет прототипно наследоваться от Солдата!
+Object.setPrototypeOf(jonh, solider); // 1арг - объект, которому назначить прототип, 2арг - объект, прототип которого устанавливаем!
 
-
-/* const numbers = {
-	a: 2,
-	b: 5,
-	c: {
-		x: 7,
-		y: 4
-	}
-};
-
-const add = {
-	d: 17,
-	e: 20 
-}; */
-
-/* console.log(Object.assign(number, add)); */
-/* const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-console.log(add); //20
-console.log(clone); //20
- */
-
-/* const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'sadasdasd';
-
-console.log(newArray); // ["a", "sadasdasd", "c"]
-console.log(oldArray); // ["a", "b", "c"] */
-
-
-/* Спрет оператор разворачивает все свойства массива */
-const video = ['youtube', 'vimeo', 'rutube'],
-	blogs = ['wordpress', 'livejornal', 'blogger'],
-	internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-	console.log(a);
-	console.log(b);
-	console.log(c);
-}
-
-const num = [2, 5, 7]; //  С него нужно получить свойства и ввести их в качестве аргументы в функции выше!
-
-log(...num); // 2, 5, 7
-
-
-// 4й способ создания поверхностой копии объекта
-
-const array = ['a', 'b'];
-const newArray = [...array];
-console.log(newArray); // ["a", "b"]
-console.log(array); // ["a", "b"]
-
-const q = {
-	one: 1,
-	two: 2
-};
-const newObj = { ...q };
-console.log(newObj); // {one: 1, two: 2}
-console.log(q); //{one: 1, two: 2}
+jonh.sayHello();
