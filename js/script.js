657463d23ba72e248f7d8c7f7c3f50715e6103dc
Viktor <example@gmail.com>
2021-02-17 14:13:25 +0600

    Урок 28 (Получение элементов на странице)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,23 @@
 "use strict";
 
+//! Старый методы!
 
-//! to String
-// 1)
-console.log(typeof (String(null))); // string
-console.log(typeof (String(4))); // string
-// 2)
-console.log(typeof (null + '')); //string
+const box = document.getElementById('box'); // старый метод
+console.log(box);
 
-const num = 5;
-console.log("https://vk.com/catalog/" + num);
+const btns = document.getElementsByTagName('button'); // Получаем псевдомассив
+console.log(btns[1]);
 
-const fontSize = 26 + 'px';
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-//! To Number 
-// 1)
-console.log(typeof (Number(4))); // number
-// 2)
-console.log(typeof (+'5')); // number
-// 3)
-console.log(typeof (parseInt("15px", 10)));  // number
+//! Новые методы!
 
-let answ = +prompt('Hello!', ''); // Number
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach(item => {
+	console.log(item);
+});
+console.log(hearts);
 
-//! To Boolean 
-/* 0, '', null, undefined, NaN --- False */
-
-let switcher = null;
-if (switcher) {
-	console.log('working...');
-}
-switcher = 1;
-if (switcher) {
-	console.log('working...');
-}
-// 2)
-console.log(typeof (Boolean('4'))); // Boolean
-// 3)
-console.log(typeof (!!'4')); // True
+const oneHeart = document.querySelector('div');
+console.log(oneHeart);
