c7617c4c09523ffdb4dc52f0ba19435e9d617152
Viktor <example@gmail.com>
2021-02-17 15:50:24 +0600

    Урок 29 (Действия с элементами на странице)

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
 "use strict";
 
-//! Старый методы!
+const box = document.getElementById('box'),
+	btns = document.getElementsByTagName('button'),
+	circles = document.getElementsByClassName('circle'),
+	hearts = document.querySelectorAll('.heart'),
+	oneHears = document.querySelector('.heart'),
+	wrapper = document.querySelector('.wrapper');
 
-const box = document.getElementById('box'); // старый метод
-console.log(box);
+// box.style.backgroundColor = 'blue';
 
-const btns = document.getElementsByTagName('button'); // Получаем псевдомассив
-console.log(btns[1]);
+// box.style.cssText = 'background-color: blue; width: 500px;';
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+// btns[1].style.borderRadius = '100%';
+// circles[0].style.backgroundColor = 'red';
 
-//! Новые методы!
+// for (let i = 0; i < hearts.length; i++) {
+// 	hearts[i].style.backgroundColor = 'blue';
+// }
 
-const hearts = document.querySelectorAll('.heart');
-hearts.forEach(item => {
-	console.log(item);
-});
-console.log(hearts);
+// hearts.forEach(item => {
+// 	item.style.backgroundColor = 'blue';
+// });
 
-const oneHeart = document.querySelector('div');
-console.log(oneHeart);
+// Создание тега через JS
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я!');
+div.classList.add('black');
+
+wrapper.append(div); // Новый метод! вставляет элемент в конец дочерних у родителя
+// wrapper.appendChild(div); // Старый метод!  вставляет элемент в конец дочерних у родителя
+
+// wrapper.prepend(div);//Новый метод! вставляет элемент в начале дочерних у родителя
+
+// hearts[0].before(div); Новый метод!
+// hearts[0].after(div); Новый метод!
+
+// wrapper.insertBefore(div, hearts[1]); // Старый метод! 
+
+// circles[0].remove(); // Новый метод! удаляет элемент
+// wrapper.removeChild(hearts[1]); // Старый метод удаления элемента!
+
+// hearts[0].replaceWith(circles[0]); // Новый метод! Который меняем, на который меняем!
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello World!</h1>";
+
+// div.textContent = "Hello!";
+
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
