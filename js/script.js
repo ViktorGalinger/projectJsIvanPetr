979be4e65bc7ae77a96812bc45ad991b44faac81
Viktor <example@gmail.com>
2021-02-17 11:26:57 +0600

    Урок 26 (Динамическая типизация)

--- js/script.js
+++ js/script.js
@@ -1,79 +1,40 @@
-/* Задание на урок:
+"use strict";
 
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+//! to String
+// 1)
+console.log(typeof (String(null))); // string
+console.log(typeof (String(4))); // string
+// 2)
+console.log(typeof (null + '')); //string
 
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+const num = 5;
+console.log("https://vk.com/catalog/" + num);
 
-'use strict';
+const fontSize = 26 + 'px';
 
-const personalMovieDB = {
-	count: 0,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-	start: function () {
-		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//! To Number 
+// 1)
+console.log(typeof (Number(4))); // number
+// 2)
+console.log(typeof (+'5')); // number
+// 3)
+console.log(typeof (parseInt("15px", 10)));  // number
 
-		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-		}
-	},
-	rememberMyFilms: function () {
-		for (let i = 0; i < 2; i++) {
-			let a = prompt('Один из последних просмотренных фильмов?', '');
-			let b = prompt('На сколько оцените его?', '');
+let answ = +prompt('Hello!', ''); // Number
 
-			if (a != '' && b != '' && a != null & b != null && a.length < 50) {
-				personalMovieDB.movies[a] = b;
-			} else {
-				i--;
-			}
-		}
-	},
-	detectPersonalLevel: function () {
-		if (personalMovieDB.count < 10) {
-			alert('Просмотрено довольно мало фильмов');
-		} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-			alert('Вы классический зритель');
-		} else if (personalMovieDB.count > 30) {
-			alert('Вы киноман');
-		} else {
-			alert('Произошла ошибка');
-		}
-	},
-	showMyDB: function (hidden) {
-		if (!hidden) {
-			console.log(personalMovieDB);
-		}
-	},
-	toggleVisibleMyDB: function () {
-		if (personalMovieDB.privat) {
-			personalMovieDB.privat = false;
-		} else {
-			personalMovieDB.privat = true;
-		}
-	},
-	writeYourGenres: function () {
-		for (let i = 1; i <= 3; i++) {
-			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-			if (genre === '' || genre == null) {
-				console.log("Некорректно ввели данные или не ввели их вовсе!");
-				i--;
-			} else {
-				personalMovieDB.genres[i - 1] = genre;
-			}
-		}
-		personalMovieDB.genres.forEach((item, i) => {
-			console.log(`Любимый жанр ${i + 1} - это ${item}`);
-		});
-	}
-};
+//! To Boolean 
+/* 0, '', null, undefined, NaN --- False */
+
+let switcher = null;
+if (switcher) {
+	console.log('working...');
+}
+switcher = 1;
+if (switcher) {
+	console.log('working...');
+}
+// 2)
+console.log(typeof (Boolean('4'))); // Boolean
+// 3)
+console.log(typeof (!!'4')); // True
